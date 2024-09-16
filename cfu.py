0afaf48cccf9dfe15b1f6c239dc2ce306b1a10b3
NATALYP29 <144722471+NATALYP29@users.noreply.github.com>
2024-09-16 09:20:32 -0400

    Create cfu.py

---
+++ cfu.py
@@ -0,0 +1,6 @@
+fname = input("What is your first name?")
+lname = input("What is you last name?")
+age = input("How old are you?")
+school = input("How long have you been going to school for?")
+
+print (fname, lname, age, school)
